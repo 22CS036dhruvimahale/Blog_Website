60648960ef45c7591f682b985d39072e2b7e1d6d
22CS036dhruvimahale <mahaledhruvi08@gmail.com>
2024-07-28 14:10:12 +0530

    Created Schema

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,8 +1,20 @@
-export const signupUser =(request,response) =>{
+
+import User from '../model/user.js';
+
+
+
+export const signupUser =async(request,response) =>{
     try{
         const user= request.body;
+
+        const newUser=new User(user);
+    await newUser.save();
+
+    return response.status(200).json({msg:'Signup successful'})
     }
 catch(error){
+
+    return response.status(500).json({msg:'Error while signing up the user'});
 
 } 
 }
