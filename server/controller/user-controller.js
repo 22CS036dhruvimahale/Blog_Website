630ef5f0f6b00a89c8d755547339fb50b26ddd74
22CS036dhruvimahale <mahaledhruvi08@gmail.com>
2024-09-03 19:28:05 +0530

    backend done for login

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -8,25 +8,15 @@ import User from '../model/user.js';
 export const signupUser = async (req, res) => {
   try {
 const user =request.body ;
-  
 
-    
-//  const user = {username:req.body.username, name:req.body.name, password:hashedPassword}
-    
-  
-    // //Check if the username already exists
-    // const existingUser = await User.findOne({ username });
-    // if (existingUser) {
-    //   return res.status(400).json({ message: 'Username already taken' });
-    // }
 
-//     // Create a new user
+    // Create a new user
     const newUser = new User(user);
     await newUser.save();
 
-    return res.status(201).json({ message: 'User created successfully' });
+    return res.status(200).json({ message: 'User created successfully' });
   } catch (error) {
-//     console.error('Error during signup:', error);
+//    
     return res.status(500).json({ message: 'Something went wrong. Please try again later.' });
   }
 };
