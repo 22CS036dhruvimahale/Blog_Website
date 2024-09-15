91aa8c4e07d574e75eb8d88d754c3db7df62d345
22CS036dhruvimahale <mahaledhruvi08@gmail.com>
2024-09-15 16:09:23 +0530

    added routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 // import logo from './logo.svg';
 import './App.css';
+import DataProvider from './context/DataProvider';
+
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 //component
-
-import DataProvider from './context/DataProvider';
 
 import Login from './components/accounts/login';
 
@@ -11,14 +12,26 @@ import Home from './components/home/Home';
 
 function App() {
   return (
-    <div style={{marginTop:64}}>
+   
+   <DataProvider>  
 
-     <DataProvider>   
-     <Login/>
-     <Home/>
-     </DataProvider>
+{/* enables routing in the whole browser */}
+      <BrowserRouter>
+        <div style={{marginTop:64}}>
+
+          <Routes> {/* to add routing to each page*/}
+         
+            <Route path='/login' element={<Login />} />
+            <Route path='/' element={<Home/>} />
+
+          </Routes>
+       </div> 
+     </BrowserRouter>
+    
+    
+  </DataProvider>
       
-        </div>
+       
   );
 }
 
