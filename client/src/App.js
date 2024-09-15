e1c4dde255800881b980f96f06cc491f41bc0f6d
22CS036dhruvimahale <mahaledhruvi08@gmail.com>
2024-09-15 17:27:02 +0530

    user authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,24 @@ import { useState } from 'react';
 
 import DataProvider from './context/DataProvider';
 
-import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route,Outlet,Navigate} from 'react-router-dom';
 
 //component
 import Login from './components/accounts/login';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+
+
+
+const PrivateRoute = ({ isAuthenticated, ...props }) => {
+  // const token = sessionStorage.getItem('accessToken');
+  return isAuthenticated ? 
+    <>
+      <Header/>
+      <Outlet />
+    </>
+     : <Navigate replace to='/login' />
+};
 
 function App() {
   const [isAuthenticated, isUserAuthenticated] = useState(false);
@@ -16,21 +28,21 @@ function App() {
    
    <DataProvider>  
       <BrowserRouter>       {/* enables routing in the whole browser */}
-       <Header/>
+      
          <div style={{marginTop:64}}>
 
           <Routes> {/* to add routing to each page*/}
             <Route path='/login' element={<Login isUserAuthenticated={isUserAuthenticated} />} />
-            <Route path='/' element={<Home/>} />
+            
+            <Route path='/' element={<PrivateRoute isAuthenticated={isAuthenticated} />} >
+               <Route path='/' element={<Home/>} />
+            </Route>
+
           </Routes>
 
         </div> 
       </BrowserRouter>
-    
-    
   </DataProvider>
-      
-       
   );
 }
 
